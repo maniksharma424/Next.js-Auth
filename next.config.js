86ab2da1746fe8e27d5b370e5949c98ca617a0da
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-07-14 13:24:42 +0530

    next-image-config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:["https://icons-for-free.com/iconfiles/png/512/color+google+media+network+social+icon-1320086080668511532.png","https://seeklogo.com/images/A/apple-logo-52C416BDDD-seeklogo.com.png","https://tricky-photoshop.com/wp-content/uploads/2017/08/6.jpg"]
 }
 
 module.exports = nextConfig
